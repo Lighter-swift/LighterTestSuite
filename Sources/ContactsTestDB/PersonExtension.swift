8eb44cbc5c596fdedddcbac72dfa3f6f7fb592e0
Helge Heß <me@helgehess.eu>
2022-08-05 22:21:02 +0200

    Play w/ any extension
    ...

--- Sources/ContactsTestDB/PersonExtension.swift
+++ Sources/ContactsTestDB/PersonExtension.swift
@@ -3,7 +3,11 @@
 //  Copyright © 2022 ZeeZide GmbH.
 //
 
-extension ContactsDB {
+extension Person {
   
-  
+  var fullName : String {
+    guard let fn = firstname, !fn.isEmpty else { return lastname }
+    guard !lastname.isEmpty else { return fn }
+    return "\(fn) \(lastname)"
+  }
 }
